4ce160b985094a48d984f212abe9ccbd9a90c5c1
Tim Hagemann <tim@eternalwings.de>
2021-08-27 11:17:22 +0200

    Provide conversion functions for generic overlays for known units
    This is a first step into providing more useful conversions for units that we
    directly know, as we do for temperature and wind. Currently, we just show the
    unit that was provided by the variable responsible for an overlay without any
    conversions to other, related units. While we cannot provide conversions for all
     kinds of units, this adds initial support for some simple units, specifically
    temperature. However, this could be extended to other units that make sense.

--- src/products.js
+++ src/products.js
@@ -13,6 +13,27 @@ import { floatToDate } from "./date";
 import { degreeToIndexWithStepCount, floorMod, radiansToDegrees } from "./math";
 import µ from './micro';
 
+const TEMPERATURE_UNITS = [
+    {
+        label: "°C", conversion: (x) => x - 273.15, precision: 1
+    },
+    {
+        label: "°F", conversion: (x) => x * 9 / 5 - 459.67, precision: 1
+    },
+    {
+        label: "K", conversion: (x) => x, precision: 1
+    }
+];
+
+const DISTANCE_UNITS = [
+    {
+        label: "m", conversion: (x) => x, precision: 1
+    },
+    {
+        label: "ft", conversion: (x) => x * 3.2808, precision: 1
+    }
+];
+
 function buildProduct(overrides) {
     return _.extend({
         description: "",
@@ -44,6 +65,48 @@ function localize(table) {
         });
         return result;
     }
+}
+
+/**
+ * Tries to find units that are similar to the given unit and sets up the conversions
+ * between these units. If no matching units are found, only the given unit will be
+ * configured and returned.
+ *
+ * @param unit The specified unit of the variable
+ * @param min The min value of the variable in the given unit
+ * @param max The max value of the variable in the given unit
+ * @returns list of units available that can be displayed for the passed unit
+ */
+function findMatchingUnitConversations(unit, min, max) {
+    if(unit === "K") {
+        return TEMPERATURE_UNITS;
+    } else if(["C", "F", "°C", "°F"].includes(unit)) {
+        let toKelvinConversion;
+        if(["°C", "C"].includes(unit)) {
+            toKelvinConversion = (x) => x + 273.15;
+        } else {
+            toKelvinConversion = (x) => (x - 32) / 1.8 + 273.15;
+        }
+
+        return TEMPERATURE_UNITS.map(units => {
+            return {
+                ...units,
+                conversion: (x) => {
+                    const kelvin = toKelvinConversion(x);
+                    return units.conversion(kelvin);
+                },
+                precision: findRequiredPrecision(units.conversion(min), units.conversion(max))
+            }
+        });
+    } else if(unit === "m") {
+        return DISTANCE_UNITS;
+    }
+
+    return [{
+        label: unit,
+        conversion: (x) => x,
+        precision: findRequiredPrecision(min, max)
+    }];
 }
 
 function createIrregularHeader(metadata, time, gridDescription) {
@@ -492,11 +555,7 @@ const FACTORIES = {
 
                     return {
                         header,
-                        units: [{
-                            label: unit,
-                            conversion: (x) => x,
-                            precision: findRequiredPrecision(min, max)
-                        }],
+                        units: findMatchingUnitConversations(unit, min, max),
                         interpolate: bilinearInterpolateScalar,
                         data: function(i) {
                             return values[i];
